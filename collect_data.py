e8b86e9c9b7dccebbc54de6e41ec7bc5e5c0e85f
Jennifer Wang <jen@jewang.net>
2018-10-13 18:46:59 -0700

    Some initial data

--- collect_data.py
+++ collect_data.py
@@ -2,23 +2,31 @@ import board
 import busio
 import adafruit_bno055
 import time
-import numpy as np
+import datetime
+import numpy
+import pandas as pd
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055(i2c)
 
 i = 0
 header = "time_ms\ttemp_c\taccel_ms\tmag_uT\tgyro_degs\t\euler_deg\tquaternion\tlin_accel_ms\tgravity_ms\n"
-while True:
-    # print('Temperature: {} degrees C'.format(sensor.temperature))
-    # print('Accelerometer (m/s^2): {}'.format(sensor.accelerometer))
-    # print('Magnetometer (microteslas): {}'.format(sensor.magnetometer))
-    # print('Gyroscope (deg/sec): {}'.format(sensor.gyroscope))
-    # print('Euler angle: {}'.format(sensor.euler))
-    # print('Quaternion: {}'.format(sensor.quaternion))
-    # print('Linear acceleration (m/s^2): {}'.format(sensor.linear_acceleration))
-    # print('Gravity (m/s^2): {}'.format(sensor.gravity))
+data = []
 
-    time.sleep(100)
-    file_name = "data" + i + ".csv"
-    input("Press Enter to continue...")
+for i in range(10):
+  input("Press Enter to continue...")
+  start = datetime.datetime.now()
+  elapsed_ms = 0
+  while elapsed_ms < 5000:
+    row = [elapsed_ms, sensor.temperature, sensor.accelerometer,
+           sensor.magnetometer,
+           sensor.gyroscope, sensor.gyroscope, sensor.euler,
+           sensor.quaternion, sensor.linear_acceleration, sensor.gravity]
+    data.append(row)
+    time.sleep(1 / 60.) # 16ms
+    elapsed_ms = (datetime.datetime.now() - start).total_seconds() * 1000
+
+  file_name = "data" + str(i) + ".csv"
+  df = pd.DataFrame.from_dict(data)
+  df.to_csv(file_name, header=True, index=True, mode='a')
+
