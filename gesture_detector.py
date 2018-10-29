3ab94e52e9d68b43c3553555967007a5ba91ac4f
Jennifer Wang <jenniferwang@google.com>
2018-10-28 18:19:39 -0700

    First pass detector

---
+++ gesture_detector.py
@@ -0,0 +1,60 @@
+import busio
+import time
+import datetime
+import pandas as pd
+import os
+from Adafruit_BNO055 import BNO055
+import utils
+import collections
+from sklearn.externals import joblib
+
+
+model = joblib.loads('models/102pt_model.joblib')
+
+SAMPLE_RATE_HZ = 100
+QUATERNION_SCALE = (1.0 / (1 << 14))
+
+CHECK_TIME_INCREMENT_MS = 200
+SAMPLE_SIZE_MS = 1500
+
+bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+
+# Initialize the BNO055 and stop if something went wrong.
+if not bno.begin():
+  raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+
+# Print system status and self test result.
+status, self_test, error = bno.get_system_status()
+print('System status: {0}'.format(status))
+print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+# Print out an error if system status is in error mode.
+if status == 0x01:
+  print('System error: {0}'.format(error))
+  print('See datasheet section 4.3.59 for the meaning.')
+
+i = 0
+header = ["time_ms"] + utils.get_sensor_headers()
+data = collections.deque(maxlen=SAMPLE_SIZE_MS)
+
+print('Starting operation')
+
+start = datetime.datetime.now()
+elapsed_ms = 0
+previous_elapsed_ms = 0
+last_classified = 0
+
+while True:
+  row = [elapsed_ms] + utils.read_sensors(bno)
+  data.append(row)
+
+  if elapsed_ms - last_classified >= CHECK_TIME_INCREMENT_MS:
+    df = pd.DataFrame(data, columns=header)
+    features = utils.get_all_features(df)
+
+    print df
+    print pd.concat([utils.get_all_features(df, generate_feature_names=True),
+                     features], axis=1)
+    print model.predict(df)
+
+  previous_elapsed_ms = elapsed_ms
+  elapsed_ms = (datetime.datetime.now() - start).total_seconds() * 1000
